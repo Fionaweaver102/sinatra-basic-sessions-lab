452830bc71df4bd182ca60abddc669539284d15d
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-12-25 13:21:48 -0800

    commit tree 87b53069d63f254f79a5252465e79e06faf4d4cc

---
+++ models/checkout.rb
@@ -0,0 +1,13 @@
+# class Checkout 
+#   attr_accessor :item 
+
+#   def initialize(params)
+#     @item = params[:item] 
+#     @@all << self 
+#   end 
+
+#   def self.all
+#     @@all 
+#   end 
+
+# end 
